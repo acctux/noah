d9950609753e00c119b0eb9f8b2454f1d74fc69c
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-30 02:29:44 -0400

    debug

--- inside/root-env.sh
+++ inside/root-env.sh
@@ -70,36 +70,6 @@ install_icons() {
   parallel --jobs 4 "sd '$old_color' '$new_color' {}" ::: "${matches[@]}"
 }
 
-pass_files_to_user() {
-  rsync -a "$HOME/$INSTALL_SCRIPT/" "$USER_HOME/$INSTALL_SCRIPT/"
-  rsync -a "$HOME/$KEY_DIR/" "$USER_HOME/$KEY_DIR/"
-
-  chmod 600 "$USER_HOME/$KEY_DIR/$SSH_KEYFILE"
-  chmod 600 "$USER_HOME/$KEY_DIR/$GPG_KEYFILE"
-  chown -R "$USER_NAME:$USER_NAME" "$USER_HOME/$INSTALL_SCRIPT" "$USER_HOME/$KEY_DIR"
-}
-
-create_autostart() {
-  local autostart_dir="$USER_HOME/.config/autostart"
-  local desktop_file="$autostart_dir/post.desktop"
-  local post_script="$USER_HOME/$INSTALL_SCRIPT/no_moah"
-  local exec_command="alacritty -e \"$post_script\""
-
-  mkdir -p "$autostart_dir"
-  chmod +x "$post_script"
-
-  cat >"$desktop_file" <<EOF
-[Desktop Entry]
-Type=Application
-Name=Final touches
-Exec=sh -c 'sleep 5 && $exec_command'
-Terminal=false
-NoDisplay=true
-EOF
-
-  success "Created: $desktop_file"
-}
-
 #######################################
 # Main
 #######################################
