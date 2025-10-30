7ffbdecd4b1083c681b7306dc683e2a9ab4071eb
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-30 01:40:09 -0400

    debug

--- outside/pass-info.sh
+++ outside/pass-info.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 #######################################
+# Append all globals to sensitive_conf
+#######################################
+write_secret_conf() {
+  if [[ "$BOOTLOADER" == "systemd-boot" ]]; then
+    ROOT_UUID=$(blkid -s UUID -o value "$ROOT_PARTITION")
+  fi
+  cat >"${TMP_CONF}" <<EOF
+SWORDPAS=${SWORDPAS}
+CPU_VENDOR=${CPU_VENDOR}
+GPU_VENDOR=${GPU_VENDOR}
+ISO=${ISO}
+ROOT_UUID=${ROOT_UUID}
+EOF
+}
+
+#######################################
 # Copy configuration and key files to the target system
 # Globals:
 #   INSTALL_SCRIPT, KEY_DIR
@@ -16,24 +32,7 @@ rsync_files_sys() {
   success "Configuration files transferred successfully."
 }
 
-#######################################
-# Append all globals to sensitive_conf
-#######################################
-write_secret_conf() {
-  if [[ "$BOOTLOADER" == "systemd-boot" ]]; then
-    ROOT_UUID=$(blkid -s UUID -o value "$ROOT_PARTITION")
-  fi
-  cat >"${TMP_CONF}" <<EOF
-SWORDPAS=${SWORDPAS}
-CPU_VENDOR=${CPU_VENDOR}
-GPU_VENDOR=${GPU_VENDOR}
-ISO=${ISO}
-ROOT_UUID=${ROOT_UUID}
-TMP_CONF="${TMP_CONF}"
-EOF
-}
-
 pass_files_to_sys() {
-  rsync_files_sys
   write_secret_conf
+  rsync_files_sys
 }
