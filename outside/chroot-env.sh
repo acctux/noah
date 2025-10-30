c17bb25323455e5b96c4aa999abc451e3ab7f7a3
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-29 23:14:37 -0400

    debug

--- outside/chroot-env.sh
+++ outside/chroot-env.sh
@@ -13,7 +13,6 @@
 set -euo pipefail
 
 SCRIPT_D="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
-
 PKG_D="$SCRIPT_D/pkg"
 IN_D="$SCRIPT_D/inside"
 OUT_D="$SCRIPT_D/outside"
@@ -21,6 +20,7 @@ USER_D="$SCRIPT_D/user"
 LOG_FILE="$SCRIPT_D/log"
 USER_CONF="$SCRIPT_D/user-config"
 SHARED_UTILS="$SCRIPT_D/helper-functions"
+
 #######################################
 # Sourcing
 #######################################
