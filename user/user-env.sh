825a2373e89a748a853b734972932f3ba7a64715
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-30 16:37:18 -0400

    debug

--- user/user-env.sh
+++ user/user-env.sh
@@ -18,16 +18,15 @@ set -euo pipefail
 # # Sourcing # # # # # # # # # # # # #
 #######################################
 SCRIPT_D="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
-PKG_D="${SCRIPT_D}/pkg"
-USER_SCRIPTS="${SCRIPT_D}/user/user-scripts"
+PKG_D="$SCRIPT_D/pkg"
+USER_SCRIPTS="$SCRIPT_D/user/user-scripts"
 LOG_FILE="$SCRIPT_D/log"
 
 . "$SCRIPT_D/user-config"
 
 #######################################
-# Function Sourcing
+# Variable Sourcing
 #######################################
-. "$SCRIPT_D/utils"
 . "$USER_SCRIPTS/user-icons.sh"
 . "$USER_SCRIPTS/user-services.sh"
 . "$USER_SCRIPTS/security-keys.sh"
@@ -36,26 +35,26 @@ LOG_FILE="$SCRIPT_D/log"
 #  Main
 #######################################
 main() {
-	trap 'error_trap $LINENO "$BASH_COMMAND"' ERR
-	set -eEuo pipefail
+  trap 'error_trap $LINENO "$BASH_COMMAND"' ERR
+  set -eEuo pipefail
 
-	user_folders_icons
+  user_folders_icons
 
-	info "cloning dotfiles"
-  git clone $DOT_URL $HOME/temp_dots
+  info "cloning dotfiles"
+  git clone "$DOT_URL" $HOME/temp_dots
   rsync -avh --no-perms --no-owner --no-group \
     --exclude ".git/" "$HOME/temp_dots/" "$HOME/"
   python "$USER_SCRIPTS/dotsync.py"
-	enable_user_services
+  enable_user_services
 
-	paru -S ayugram-desktop-bin surfshark-client
-	sudo firewall-cmd --set-default-zone=block
+  paru -S ayugram-desktop-bin surfshark-client
+  sudo firewall-cmd --set-default-zone=block
   sudo python "$USER_SCRIPTS/dotsync.py"
 
-	import_ssh_keys
-	import_gpg_key
-	clone_gits
-	echo -ne "
+  import_ssh_keys
+  import_gpg_key
+  clone_gits
+  echo -ne "
    -------------------------------------------------------------------------------------
    ███╗   ██╗  ██████╗   █████╗  ██╗  ██╗  ██████╗     █████╗  ██████╗   ██████╗ ██╗  ██╗
    ████╗  ██║ ██╔═══██╗ ██╔══██╗ ██║  ██║ ██╔════╝    ██╔══██╗ ██╔══██╗ ██╔════╝ ██║  ██║
@@ -68,5 +67,6 @@ main() {
                             Automated Arch Linux Installer
    -------------------------------------------------------------------------------------
                                       Done
+"
 }
 main "$@"
