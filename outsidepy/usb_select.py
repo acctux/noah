e15b26fa8d5ccd50b07afffe88f261668be12e67
Nick A <226946345+acctux@users.noreply.github.com>
2025-11-01 10:55:02 -0400

    p

--- outsidepy/usb_select.py
+++ outsidepy/usb_select.py
@@ -167,6 +167,7 @@ def unmount_partition():
 def mnt_cp_keys(KEY_DIR, KEY_FILES):
     if KEY_DIR and KEY_FILES:
         missing_files = check_usb_files(KEY_DIR, KEY_FILES)
+        log.info(f"{missing_files}")
         if missing_files:
             mount_selected(prompt_user_selection(find_keys_partition(get_lsblk_json())))
             copy_keys(KEY_DIR, KEY_FILES)
