db17233b72306344c52d6674d50ac4e0f2b89047
Nick A <226946345+acctux@users.noreply.github.com>
2025-11-01 04:03:46 -0400

    d

--- py/scripts/setup_disk.py
+++ py/scripts/setup_disk.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
 import subprocess
-from py.main import EFI_DEFAULT, FS_TYPES
+from main import EFI_DEFAULT, FS_TYPES
 from scripts.my_log import log
 import re
 
