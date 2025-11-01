47b25293274ba20273ae0040954102255c60fe0a
Nick A <226946345+acctux@users.noreply.github.com>
2025-11-01 11:14:33 -0400

    p

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def inside_env():
 
 def main():
     # outside_env()
-    arch_chroot_run(["python3", "/root/noah/inside/inside_env.sh"])
+    arch_chroot_run(["python3", "/root/noah/inside/inside-env.sh"])
 
 
 main()
