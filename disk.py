efc7e16d2e91b8d3a3dd98c6a2a0ca693202f98d
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-07 01:06:48 -0400

    p

--- disk.py
+++ disk.py
@@ -29,7 +29,8 @@ def find_disk_with_fat32(devices):
         return
     for disk in devices:
         print(f"Checking disk: {disk.device_info.path}")
-        print(disk)
+        if "sda" in disk.device_info.path:
+            return
         for part in disk.partition_infos:
             if part.fs_type.name == "FAT32":
                 print(f"Found FAT32 partition: {part.path}")
