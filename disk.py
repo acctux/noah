ac8ac44db64bf2ce568bb1bdc08ad83a0471c741
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-07 02:51:42 -0400

    p

--- disk.py
+++ disk.py
@@ -19,6 +19,7 @@ from pathlib import Path
 #########################
 def create_disk_config():
     devices = device_handler.devices
+    device_handler.umount_all_existing(Path("/mnt"))
     target_disk = ""
     for disk in devices:
         print(f"Checking disk: {disk.device_info.path}")
