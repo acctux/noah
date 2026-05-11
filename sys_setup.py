566c68c65827b804c4a1fc6c3c7e10576eca2835
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-11 04:09:29 -0400

    p

--- sys_setup.py
+++ sys_setup.py
@@ -683,7 +683,7 @@ def perform_installation(
 
 def sys_setup() -> None:
     nc = NoahConfig.from_config(ec.json_config)
-    mnt_cp_keys(nc.file_cp_list, nc.dir_cp_list)
+    mnt_cp_keys(nc.files_cp_list, nc.dir_contents_to_cp)
     arch_config_handler = ArchConfigHandler()
     arch_config = ArchConfig.from_config(
         ec.arch_config_json, Arguments(creds=Path("/root/.ssh/users.json"))
