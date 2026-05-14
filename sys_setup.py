95a29b3e0a265688c9294f632578a5d20bbf2308
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-14 16:00:32 -0400

    p

--- sys_setup.py
+++ sys_setup.py
@@ -539,11 +539,11 @@ def install_limine(installation: Installer):
             "snapper-timeline.timer",
         ]
     )
-    installation.arch_chroot("limine-install")
+    # installation.arch_chroot("limine-install")
     # installation.arch_chroot("limine-update")
-    # installation.arch_chroot(
-    #     "limine-entry-tool --add-efi 'Arch' '/boot/EFI/arch-limine/BOOTX64.EFI' --overwrite --quiet"
-    # )
+    installation.arch_chroot(
+        "limine-entry-tool --add-efi 'Arch' '/boot/EFI/arch-limine/BOOTX64.EFI' --overwrite --quiet"
+    )
 
 
 ###################################
