a114c7a1344c2dffc10f6eb77a18cc331887efd1
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-14 04:41:13 -0400

    p

--- sys_setup.py
+++ sys_setup.py
@@ -236,7 +236,14 @@ def chaotic_repo(installation: Installer) -> None:
     web = "https://cdn-mirror.chaotic.cx/chaotic-aur/"
     cmds = [
         ["pacman-key", "--init"],
-        ["pacman-key", "--add", "/root/chaotic.key"],
+        [
+            "pacman-key",
+            "--recv-key",
+            "3056513887B78AEB",
+            "--keyserver",
+            "keyserver.ubuntu.com",
+        ],
+        # ["pacman-key", "--add", "/root/chaotic.key"],
         ["pacman-key", "--lsign-key", "3056513887B78AEB"],
         ["pacman", "-U", "--noconfirm", f"{web}chaotic-keyring.pkg.tar.zst"],
         ["pacman", "-U", "--noconfirm", f"{web}chaotic-mirrorlist.pkg.tar.zst"],
