87ce9d7ad13d7f0b6b812bf813466f4c2332facb
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-18 07:17:37 -0400

    p

--- lib/init_setup.py
+++ lib/init_setup.py
@@ -122,19 +122,30 @@ def get_gfx_drivers(graphics_devices: dict[str, str]) -> list[GfxDriver]:
     ]
 
 
-def check_missing(config: NoahConfig) -> list[str]:
-    missing: list[str] = []
+def check_missing(config: NoahConfig) -> list[Path]:
+    missing: list[Path] = []
     if config.key_copy_config:
-        for _, name in config.key_copy_config.keys.items():
-            missing.append(name)
+        src_base = (
+            config.key_copy_config.resolver.usb / config.key_copy_config.source_dir
+        )
+        for name in config.key_copy_config.keys.values():
+            path = src_base / name
+            if not path.exists():
+                missing.append(path)
     if config.additional_usb_to_cp_config:
         for copy in config.additional_usb_to_cp_config.copies:
+            src_base = copy.resolver.usb / copy.source_dir
             for name in copy.names:
-                missing.append(name)
+                path = src_base / name
+                if not path.exists():
+                    missing.append(path)
     if config.dir_contents_to_cp_config:
         for copy in config.dir_contents_to_cp_config.copies:
+            src_base = copy.resolver.usb / copy.source_dir
             for name in copy.names:
-                missing.append(name)
+                path = src_base / name
+                if not path.exists():
+                    missing.append(path)
     return missing
 
 
