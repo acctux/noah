31d964658f219e5e24bf81e17042d8feb9f0916b
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-06-23 01:12:20 -0400

    p\

--- lib/init_setup.py
+++ lib/init_setup.py
@@ -95,24 +95,25 @@ def copy_root_to_mnt(mnt_point: Path, cfg: CopyConfiguration, username: str) -> 
         copy_it(src, dest)
 
 
-def copy_usb_to_root(cc: CopyConfiguration, missing: list[tuple[Path, Path]]):
+def copy_usb_to_root(usb_path: Path, missing: list[tuple[Path, Path]]):
     if not yes_no("Mount USB?"):
         return
     selected = get_device()
     if not selected:
         return
-    cc.usb.mkdir(parents=True, exist_ok=True)
-    run_dmc(["mount", "-o", "ro", str(selected), str(cc.usb)], check=True)
+    run_dmc(["mount", "-o", "ro", str(selected), str(usb_path)], check=True)
     run_dmc(["udevadm", "settle"])
     time.sleep(1)
     for src, dest in missing:
         copy_it(src, dest)
     if yes_no("Files copied, unmount?"):
-        unmount_usb(cc.usb)
+        unmount_usb(usb_path)
 
 
 def init_arch_conf(
-    arch_config_json: dict, auth_conf_path: str, arch_config_handler: ArchConfigHandler
+    arch_config_json: dict,
+    auth_conf_path: Path | None,
+    arch_config_handler: ArchConfigHandler,
 ) -> ArchConfigHandler:
     arch_config = ArchConfig.from_config(arch_config_json, Arguments(None))
     arch_config_handler.config.hostname = arch_config.hostname
@@ -128,7 +129,7 @@ def init_arch_conf(
     arch_config_handler.config.packages = arch_config.packages
     arch_config_handler.config.services = arch_config.services
     arch_config_handler.config.app_config = arch_config.app_config
-    if not Path(auth_conf_path).is_file():
+    if not auth_conf_path or not auth_conf_path.is_file():
         return arch_config_handler
     with open(auth_conf_path, "r") as f:
         users_dict = json.load(f)
@@ -139,28 +140,32 @@ def init_arch_conf(
 
 def init_setup(
     arch_config_json: dict,
-    auth_conf_path: str,
     noahconf_json: dict,
     arch_config_handler: ArchConfigHandler,
     usb_mnt: Path = Path("/mnt/usb"),
 ) -> tuple[ArchConfigHandler, NoahConfig]:
     nc = NoahConfig.from_config(noahconf_json)
     print(nc.auth_config)
-    if nc.auth_config:
-        print(nc.auth_config.resolve_usb_to_root())
     if usb_mnt.is_mount() and yes_no("USB mounted, unmount?"):
         unmount_usb(usb_mnt)
-    if nc.copy_config:
+    if nc.copy_config or nc.auth_config:
         missing = []
-        for src, dest in nc.copy_config.resolve_usb_to_root():
-            if not dest.exists():
-                missing.append((src, dest))
+        if nc.copy_config:
+            for src, dest in nc.copy_config.resolve_usb_to_root():
+                if not dest.exists():
+                    missing.append((src, dest))
+        if nc.auth_config:
+            auth_src, auth_dest = nc.auth_config.resolve_usb_to_root()
+            if not auth_dest.exists():
+                missing.append((auth_src, auth_dest))
         if missing:
             log.warning("Not present: " + ", ".join([tar.name for _, tar in missing]))
-            copy_usb_to_root(nc.copy_config, missing)
+            usb: Path = Path("/mnt/usb")
+            usb.mkdir(parents=True, exist_ok=True)
+            copy_usb_to_root(usb, missing)
         else:
             log.info("All files to copy from USB found.")
     arch_config_handler = init_arch_conf(
-        arch_config_json, auth_conf_path, arch_config_handler
+        arch_config_json, auth_dest, arch_config_handler
     )
     return arch_config_handler, nc
